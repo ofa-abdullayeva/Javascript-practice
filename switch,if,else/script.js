7cd5171e25166503deb529d393060dbf9fa02501
ofa-abdullayeva <abdullayevaofa25@gmail.com>
2021-07-09 20:32:37 +0400

    for

--- switch,if,else/script.js
+++ switch,if,else/script.js
@@ -1,22 +1,22 @@
-      //saat 5 dise kursun qapisi aciqdi,6 1saat sora gel,7 dise vaxtinda gelmisen 8 dise gecikmisen
-      let  kurs = 6
-      switch(kurs){
-          case 5:
-          console.log("Kursun qapisi aciqdi")
-          break;
-          case 6:
-          console.log("1 saat sonra gel")
-          break;
-          case 7:
-          console.log("Vaxtinda gelmisen")
-          break;
-          case 8:
-          console.log("gecikmisen")
-          break;
-          default:
-          console.log("Xos gelmisiz")
-          break;
-      }
+      // //saat 5 dise kursun qapisi aciqdi,6 1saat sora gel,7 dise vaxtinda gelmisen 8 dise gecikmisen
+      // let  kurs = 6
+      // switch(kurs){
+      //     case 5:
+      //     console.log("Kursun qapisi aciqdi")
+      //     break;
+      //     case 6:
+      //     console.log("1 saat sonra gel")
+      //     break;
+      //     case 7:
+      //     console.log("Vaxtinda gelmisen")
+      //     break;
+      //     case 8:
+      //     console.log("gecikmisen")
+      //     break;
+      //     default:
+      //     console.log("Xos gelmisiz")
+      //     break;
+      // }
 
     //   var model = 'Iphone'
 
@@ -27,3 +27,27 @@
     //   }else{
     //       console.log('Go and buy a phone');
     //   }
+
+    // for (var i = 0; i < 5; i++ ){
+    //   console.log(i + 'qurtum')
+    // }
+
+
+    // for(var i = 1; i<=10; i ++ ){
+    //   console.log(i + ' reqem')
+    // }
+
+
+    // var arr = [10,25,6,9]
+    //  var cem = 0
+    //  for(var i = 0; i<arr.length; i++){
+    //    cem +=arr[i]
+    //  };
+
+    //  console.log(cem);
+
+    var cem = 0
+    for (i=0;i<=5;i++){
+      cem +=i
+    }
+    console.log(cem)

c41d42db14a72f38ed3b34b9497810e993c26a16
ofa-abdullayeva <abdullayevaofa25@gmail.com>
2021-07-09 19:51:57 +0400

    if else switch

---
+++ switch,if,else/script.js
@@ -0,0 +1,29 @@
+      //saat 5 dise kursun qapisi aciqdi,6 1saat sora gel,7 dise vaxtinda gelmisen 8 dise gecikmisen
+      let  kurs = 6
+      switch(kurs){
+          case 5:
+          console.log("Kursun qapisi aciqdi")
+          break;
+          case 6:
+          console.log("1 saat sonra gel")
+          break;
+          case 7:
+          console.log("Vaxtinda gelmisen")
+          break;
+          case 8:
+          console.log("gecikmisen")
+          break;
+          default:
+          console.log("Xos gelmisiz")
+          break;
+      }
+
+    //   var model = 'Iphone'
+
+    //   if (model=='Samsung'){
+    //       console.log('Its a brand');
+    //   } else if(model =='Xiaomi' || model =='Nokia' || model =='Huawei'){
+    //       console.log('They are brand new models');
+    //   }else{
+    //       console.log('Go and buy a phone');
+    //   }

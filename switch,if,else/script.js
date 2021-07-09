3fedb501f0e80e371344322992d99cf973e4c39d
ofa-abdullayeva <abdullayevaofa25@gmail.com>
2021-07-10 00:47:19 +0400

    function

--- switch,if,else/script.js
+++ switch,if,else/script.js
@@ -1,4 +1,6 @@
-      // //saat 5 dise kursun qapisi aciqdi,6 1saat sora gel,7 dise vaxtinda gelmisen 8 dise gecikmisen
+     //-------------switch--------------
+
+     // //saat 5 dise kursun qapisi aciqdi,6 1saat sora gel,7 dise vaxtinda gelmisen 8 dise gecikmisen
       // let  kurs = 6
       // switch(kurs){
       //     case 5:
@@ -15,10 +17,15 @@
       //     break;
       //     default:
       //     console.log("Xos gelmisiz")
+
       //     break;
       // }
 
     //   var model = 'Iphone'
+
+
+
+    //-------------If Else--------------
 
     //   if (model=='Samsung'){
     //       console.log('Its a brand');
@@ -28,26 +35,85 @@
     //       console.log('Go and buy a phone');
     //   }
 
+    //-------------For--------------
+
     // for (var i = 0; i < 5; i++ ){
     //   console.log(i + 'qurtum')
     // }
 
 
-    // for(var i = 1; i<=10; i ++ ){
+    // for(var i = 1; i<=10; i ++ ){  // 1-den 10 qeder reqemleri yazdirmaq
     //   console.log(i + ' reqem')
     // }
 
 
     // var arr = [10,25,6,9]
     //  var cem = 0
-    //  for(var i = 0; i<arr.length; i++){
+    //  for(var i = 0; i<arr.length; i++){ // arrayleri toplamaq
     //    cem +=arr[i]
     //  };
 
     //  console.log(cem);
 
-    var cem = 0
-    for (i=0;i<=5;i++){
-      cem +=i
-    }
-    console.log(cem)
+    // var cem = 0
+    // for (i=0;i<=5;i++){    // 1-den 5 qeder reqemleri toplamaq
+    //   cem +=i
+    // }
+    // console.log(cem)
+
+  //  //---------------------for of------------------
+  //   var ededler = [5,2,36,9,20]; ededin ozunu gotururse numerable
+    
+  //   for(eded of ededler){   //butun ededleri tek tek 10 ile cemlemek
+  //     eded = eded  + 10;
+  //     console.log(eded)
+  //   }
+    
+
+    
+  //  //---------------------for in------------------
+  //  var ededler = [5,2,36,9,20]; // indexsin ozunu gotururse itereble
+    
+  //  for(eded in ededler){   //butun ededlerin indexlerini tek tek 10 ile cemlemek(string olaraq birlesdirir)
+  //    eded = eded  + 10;
+  //    console.log(eded)
+  //  }
+
+  //    var ededler = [5,2,36,9,20];
+  //    var yeniededler = ededler.slice(0,4);
+    
+  //  for(eded in yeniededler){   //butun ededlerin indexlerini tek tek 10 ile cemlemek(string olaraq birlesdirir)
+     
+  //    console.log(eded=eded + 5)
+  //  }
+
+
+
+
+
+
+
+
+  //------------------Function---------------
+
+      // function declaration  -  hoists
+
+      // function getNumber (a,b){
+      //   return a*b
+      // }
+      // console.log(getNumber(8,4))
+
+
+
+
+    //   // function expression
+    //   var foo = function(){
+    //    console.log('Good day')
+    //  }
+
+     //   foo()
+
+      //Arrow funcion
+       let x = () =>{
+
+       }

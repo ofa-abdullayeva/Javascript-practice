a9888b746aa6fbbd5be3d9069abc768622371661
ofa-abdullayeva <abdullayevaofa25@gmail.com>
2021-07-12 20:49:16 +0400

    Object Methods

---
+++ 12 iyul/script.js
@@ -0,0 +1,24 @@
+//=============Object============//
+
+
+var person = {
+    name: "Aqil",
+    age : 20,
+    position: "Angular developper"
+}
+
+// console.log(person.age)
+
+
+//===========Object keys =========//
+
+console.log(Object.keys(person))
+
+
+//============= Object  Values ===========//
+
+console.log(Object.values(person))
+
+//============= Object  Enteries ===========//
+
+console.log(Object.entries(person))
